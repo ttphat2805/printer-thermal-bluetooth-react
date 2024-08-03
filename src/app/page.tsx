aa8b546c26ec1bdff3ace0d31a822768bb7aedc2
phat.tran <phattran28052002@gmail.com>
2024-08-03 08:22:26 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,178 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 
 export default function Home() {
+  const [device, setDevice] = useState<BluetoothDevice | null>(null);
+  const [server, setServer] = useState<BluetoothRemoteGATTServer | null>(null);
+  const [connected, setConnected] = useState(false);
+  const [services, setServices] = useState<BluetoothRemoteGATTService[]>([]);
+  const [characteristics, setCharacteristics] = useState<
+    BluetoothRemoteGATTCharacteristic[]
+  >([]);
+
+  const requestDevice = async () => {
+    try {
+      const device = await navigator.bluetooth.requestDevice({
+        acceptAllDevices: true,
+        optionalServices: ['49535343-fe7d-4ae5-8fa9-9fafd205e455'],
+      });
+      console.log("Device:", device);
+      setDevice(device);
+    } catch (error) {
+      console.error("Error requesting device:", error);
+    }
+  };
+
+  const connectToDevice = async () => {
+    if (!device) return;
+    try {
+      const server = await device.gatt?.connect();
+      // console.log("Server:", server?.getPrimaryService(""));
+      setServer(server as any);
+      setConnected(true);
+      console.log("Connected to device:", device);
+    } catch (error) {
+      console.error("Error connecting to device:", error);
+    }
+  };
+
+  const discoverServicesAndCharacteristics = async () => {
+    if (!server) return;
+    try {
+      const services = await server.getPrimaryServices();
+      console.log("Services:", services);
+      setServices(services);
+
+      for (const service of services) {
+        const characteristics = await service.getCharacteristics();
+        console.log("characteristics: ", characteristics);
+        setCharacteristics(characteristics);
+      }
+    } catch (error) {
+      console.error("Error discovering services and characteristics:", error);
+    }
+  };
+
+  const printData = async (data: string) => {
+    if (!characteristics.length) return;
+    try {
+      const encoder = new TextEncoder();
+      const encodedData = encoder.encode(data);
+
+      let writeCharacteristic = null;
+      for (const characteristic of characteristics) {
+        if (
+          characteristic.properties.writeWithoutResponse &&
+          characteristic.properties.write
+        ) {
+          writeCharacteristic = characteristic;
+          break;
+        }
+      }
+
+      if (!writeCharacteristic) {
+        console.error("No writable characteristic found.");
+        return;
+      }
+
+      await writeCharacteristic.writeValue(encodedData);
+      console.log("Data sent to characteristic:", writeCharacteristic.uuid);
+    } catch (error) {
+      console.error("Error sending data to printer:", error);
+    }
+  };
+
+  const formatBill = (items: any, total: any) => {
+    let bill = "Restaurant Name\n";
+    bill += "Address Line 1\n";
+    bill += "Address Line 2\n";
+    bill += "Phone: 123-456-7890\n\n";
+    bill += "Item           Qty   Price\n";
+    bill += "--------------------------\n";
+
+    items.forEach(
+      (item: {
+        name: string;
+        qty: { toString: () => string };
+        price: number;
+      }) => {
+        bill += `${item.name.padEnd(15)} ${item.qty
+          .toString()
+          .padEnd(4)} ${item.price.toFixed(2)}\n`;
+      }
+    );
+
+    bill += "--------------------------\n";
+    bill += `Total: ${total.toFixed(2)}\n`;
+    bill += "Thank you for dining with us!\n";
+    bill += "\n\n\n";
+
+    return bill;
+  };
+
+  const printBill = () => {
+    const items = [
+      { name: "Nguyen Van Triet", qty: 2, price: 200.99 },
+      { name: "Phi Ne", qty: 10, price: 229.99 },
+      { name: "Huy Ne", qty: 20, price: 122.49 },
+    ];
+    const total = items.reduce((acc, item) => acc + item.qty * item.price, 0);
+    const bill = formatBill(items, total);
+    printData(bill);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
+      <h1 className="text-3xl font-bold mb-6">Bluetooth Printer Interface</h1>
+      <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-md">
+        <button
+          onClick={requestDevice}
+          className="w-full bg-blue-500 text-white py-2 px-4 rounded mb-4 hover:bg-blue-600"
+        >
+          Request Device
+        </button>
+        <button
+          onClick={connectToDevice}
+          className={`w-full py-2 px-4 rounded mb-4 bg-green-500 hover:bg-green-600 text-white`}
+        >
+          Connect to Device
+        </button>
+        <button
+          onClick={discoverServicesAndCharacteristics}
+          className={`w-full py-2 px-4 rounded mb-4 g-purple-500 bg-purple-600 text-white`}
+        >
+          Discover Services and Characteristics
+        </button>
+        <button
+          onClick={printBill}
+          className={`w-full py-2 px-4 rounded bg-indigo-500 hover:bg-indigo-600 text-white`}
+        >
+          Print Bill
+        </button>
+        {connected && (
+          <p className="mt-4 text-green-500">Connected to {device?.name}</p>
+        )}
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="mt-6 w-full max-w-md bg-white p-6 rounded-lg shadow-md">
+        <h3 className="text-xl font-semibold mb-4">Discovered Services</h3>
+        <ul className="list-disc pl-5">
+          {services.map((service) => (
+            <li key={service.uuid} className="mb-2">
+              {service.uuid}
+            </li>
+          ))}
+        </ul>
+        <h3 className="text-xl font-semibold mb-4">
+          Discovered Characteristics
+        </h3>
+        <ul className="list-disc pl-5">
+          {characteristics.map((characteristic) => (
+            <li key={characteristic.uuid} className="mb-2">
+              {characteristic.uuid}
+            </li>
+          ))}
+        </ul>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
